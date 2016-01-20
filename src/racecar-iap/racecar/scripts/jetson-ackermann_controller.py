350a35ec4560b5c0656db9fb0d0d1f9a6774c5f2
rmelo19 <rodrigovmm@gmail.com>
2016-01-20 00:33:01 -0500

    integrated the controller node changes

--- src/racecar-iap/racecar/scripts/jetson-ackermann_controller.py
+++ src/racecar-iap/racecar/scripts/jetson-ackermann_controller.py
@@ -73,7 +73,7 @@ class _AckermannCtrlr(object):
 
         # Publishers
         self._ackermann_cmd_pub = rospy.Publisher("/vesc/ackermann_cmd", AckermannDriveStamped, queue_size=10)
-	self._ki_val_pub = rospy.Publisher("/vesc/ki_val", Float64, queue_size=10)
+        self._ki_val_pub = rospy.Publisher("/vesc/ki_val", Float64, queue_size=10)
 
         # Subscribers
         # laser data
@@ -81,15 +81,14 @@ class _AckermannCtrlr(object):
         # wall data
         rospy.Subscriber("/vesc/wd", wd, self.wall_callback, queue_size=20)
         # danger data
-        rospy.Subscriber("/vesc/danger", Bool, self.danger_callback, queue_size=20)
+        rospy.Subscriber("/vesc/danger", Float64, self.danger_callback, queue_size=20)
         # joy data
         rospy.Subscriber("/vesc/joy", Joy, self.joy_callback, queue_size=20)
 
         self.sign = 1
-        self.danger = False
+        self.danger = 0
         self.joy_command = [0]*12
-        self._int_error_sum_a = 0
-        self._int_error_sum_b = 0
+        self._int_error_sum = 0
         self._dist_right_wall = 0
         self._dist_left_wall = 0
         self._has_corner = False
@@ -97,10 +96,12 @@ class _AckermannCtrlr(object):
         self._corner_y = 0
         self._xo = 1
         self._yo = 0
+        self.dist_total = 0
+        self.parallel = 0
 
         self.ki_button_pressed = False
         self.ki_button_pressed_time = rospy.get_time()
-	self._ki = 1
+        self._ki = 1
 
     # def laser_callback(self, laser_data):
     #     # update laser data
@@ -125,25 +126,34 @@ class _AckermannCtrlr(object):
         
         self._middle_line_a = h
         self._prev_middle_line_b = self._middle_line_b
-        self._middle_line_b = k
+        self._middle_line_b = k - 0.3 # trying to correct left bias
 
         self._dist_left_wall = self._left_wall_b/self._left_wall_a * math.sin(math.atan(self._left_wall_a))
         self._dist_right_wall = -self._right_wall_b/self._right_wall_a * math.sin(math.atan(self._right_wall_a))
         self._has_corner = walls_data.corner_bool
         self._corner_x = walls_data.corner_x
         self._corner_y = walls_data.corner_y
+
+        shift = 1.5
         if self._has_corner:
-            print("Corner angle: " + str(math.atan(self._corner_y/self._corner_x)/math.pi*180))
-            print("Corner range: " + str(math.sqrt(self._corner_x**2 + self._corner_y**2)))      
-            self._xo = self._corner_x + 1.5*math.cos(math.atan(self._right_wall_a))
-            self._yo = self._corner_y + 1.5*math.sin(math.atan(self._right_wall_a))
+            # print("Corner steering_angle: " + str(math.atan(self._corner_y/self._corner_x)/math.pi*180))
+            # print("Corner range: " + str(math.sqrt(self._corner_x**2 + self._corner_y**2)))      
+            self._xo = self._corner_x + shift*math.cos(math.atan(self._right_wall_a))
+            self._yo = self._corner_y + shift*math.sin(math.atan(self._right_wall_a))
         else:
             alpha = math.atan(self._left_wall_a)
             beta = math.atan(self._right_wall_a)
             a = math.tan((alpha + beta)/2.0)
             b = (self._right_wall_b - self._left_wall_b)*(self._left_wall_a - a)/(self._left_wall_a - self._right_wall_a) + self._left_wall_b
-            self._xo = 3
-            self._yo = a*3 + b
+            self._xo = 5
+            self._yo = a*5 + b
+
+        # Distance between the walls, considering the robot position
+        dist_right = abs(self._right_wall_b)/math.sqrt(self._right_wall_a**2 + 1)
+        dist_left = abs(self._left_wall_b)/math.sqrt(self._left_wall_a**2 + 1)
+        self.dist_total = dist_right + dist_left
+        self.parallel = abs(self._right_wall_a - self._left_wall_a) < math.tan(10/180*math.pi)
+
 
         # print("Middle Angular Coefficient: " + str(self._middle_line_a))
         # print("Right Angular Coefficient: " + str(self._right_wall_a))
@@ -180,7 +190,6 @@ class _AckermannCtrlr(object):
     def danger_callback(self, danger_data):
         # update danger data
         self.danger = danger_data.data
-        pass
 
     def joy_callback(self, joy_data):
         # update danger data
@@ -213,7 +222,6 @@ class _AckermannCtrlr(object):
             if self.joy_command[4] != 1:
                 self._ackermann_cmd_pub.publish(self._ackermann_cmd)
 
-
             # Publish ki
             if self.ki_button_pressed and (t-self.ki_button_pressed_time > .15)
                 self.ki_button_pressed = False
@@ -234,14 +242,46 @@ class _AckermannCtrlr(object):
                     self.ki_button_pressed = True
                     self.ki_button_pressed_time = t
 
+
             # wait
             self._sleep_timer.sleep()
 
     def _ctrl_speed(self):
-        if self.danger or self.joy_command[4] == 1:
+        if self.joy_command[4] == 1:
             _ctrl_speed_input = 0
         else:
-            _ctrl_speed_input = 8
+            # if there is a corner, go slower
+            if self._has_corner:
+                if self.danger == 0:
+                    _ctrl_speed_input = 4
+                elif self.danger == 1:
+                    _ctrl_speed_input = 0.5
+                elif self.danger == 2:
+                    _ctrl_speed_input = 1
+                elif self.danger == 3:
+                    _ctrl_speed_input = 3
+
+                print("Speed: " + str(_ctrl_speed_input*3000))
+                # _ctrl_speed_input = 0
+
+
+                #_ctrl_speed_input = 6 if self.danger == 0 else 3000*self.danger
+            else:
+                #_ctrl_speed_input = 6 if self.danger == 0 else 3000*self.danger
+                if self.danger == 0:
+                    _ctrl_speed_input = 4#6 if self.dist_total > 3 and self.parallel else 4
+                elif self.danger == 1:
+                    _ctrl_speed_input = 0.5
+                elif self.danger == 2:
+                    _ctrl_speed_input = 1
+                elif self.danger == 3:
+                    _ctrl_speed_input = 3
+
+                print("Parallel: " + str(self.parallel))
+                print("Distance bet walls: " + str(self.dist_total))
+                print("Speed: " + str(_ctrl_speed_input*3000))
+                # _ctrl_speed_input = 0
+
 
         return _ctrl_speed_input
 
@@ -249,11 +289,20 @@ class _AckermannCtrlr(object):
         #kP = 0.5 # for low velocity
         kP = 0.2 # for high velocity
         kD = 0.1 
-        kI = self._ki
+        kI = 0 # self._ki
 
-        _ctrl_steering_input = kP*math.atan2(self._yo, self._xo)
+        self._int_error_sum = self._int_error_sum + math.atan2(self._yo, self._xo)
 
-        print("steering_angle: " + str(_ctrl_steering_input)) 
+        saturation = 0.05
+        if kI * self._int_error_sum > saturation:
+            self._int_error_sum = saturation/kI
+        elif kI * self._int_error_sum < -saturation:
+            self._int_error_sum = -saturation/kI
+
+
+        _ctrl_steering_input = kP*math.atan2(self._yo, self._xo) + kI*self._int_error_sum
+
+        # print("steering_angle: " + str(_ctrl_steering_input)) 
         if _ctrl_steering_input > 0.34:
             _ctrl_steering_input = 0.34
         elif _ctrl_steering_input < -0.34:  
